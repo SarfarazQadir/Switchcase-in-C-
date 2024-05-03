3ec96fd204d7e5ccb60f729659549c799074930c
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-05-03 21:51:26 +0500

    Add files via upload

---
+++ Conditional Statements/Conditional Statements/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Switch Case");
+start:
+Console.WriteLine("Month Details \t \n");
+Console.WriteLine("Enter a Month");
+string month = Console.ReadLine();
+switch (month)
+{
+    case "January":
+    case "March":
+    case "May":
+    case "July":
+    case "August":
+    case "October":
+    case "December":
+        Console.WriteLine("The month of: " + month + " 31 days");
+        break;
+    case "April":
+    case "June":
+    case "September":
+    case "November":
+        Console.WriteLine("The month of: " + month + " 30 days");
+        break;
+    default:
+        Console.WriteLine("Incorrect Choice");
+        goto start;
+}   
